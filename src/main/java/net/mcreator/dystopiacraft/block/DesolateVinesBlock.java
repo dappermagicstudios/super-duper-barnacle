f1961d8a79625cf30f8471454c0c215dcc4ec470
GOBI111 <Aubrey@aubreykurz1.home>
2021-07-13 08:50:28 -0400

    hello

---
+++ src/main/java/net/mcreator/dystopiacraft/block/DesolateVinesBlock.java
@@ -0,0 +1,167 @@
+
+package net.mcreator.dystopiacraft.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.BlockClusterFeatureConfig;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.dystopiacraft.DystopiacraftModElements;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
+
+@DystopiacraftModElements.ModElement.Tag
+public class DesolateVinesBlock extends DystopiacraftModElements.ModElement {
+	@ObjectHolder("dystopiacraft:desolate_vines")
+	public static final Block block = null;
+	public DesolateVinesBlock(DystopiacraftModElements instance) {
+		super(instance, 6);
+		MinecraftForge.EVENT_BUS.register(this);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustomFlower());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+	private static Feature<BlockClusterFeatureConfig> feature = null;
+	private static ConfiguredFeature<?, ?> configuredFeature = null;
+	private static class FeatureRegisterHandler {
+		@SubscribeEvent
+		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
+			feature = new Feature<BlockClusterFeatureConfig>(BlockClusterFeatureConfig.field_236587_a_) {
+				@Override
+				public boolean generate(ISeedReader world, ChunkGenerator generator, Random random, BlockPos pos, BlockClusterFeatureConfig config) {
+					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
+					boolean dimensionCriteria = false;
+					if (dimensionType == World.OVERWORLD)
+						dimensionCriteria = true;
+					if (!dimensionCriteria)
+						return false;
+					int generated = 0;
+					for (int j = 0; j < 0; ++j) {
+						BlockPos blockpos = pos.add(random.nextInt(4) - random.nextInt(4), 0, random.nextInt(4) - random.nextInt(4));
+						if (world.isAirBlock(blockpos)) {
+							BlockPos blockpos1 = blockpos.down();
+							int k = 1 + random.nextInt(random.nextInt(3) + 1);
+							k = Math.min(3, k);
+							for (int l = 0; l < k; ++l) {
+								if (block.getDefaultState().isValidPosition(world, blockpos)) {
+									world.setBlockState(blockpos.up(l), block.getDefaultState(), 2);
+									generated++;
+								}
+							}
+						}
+					}
+					return generated > 0;
+				}
+			};
+			configuredFeature = feature.withConfiguration(
+					(new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(block.getDefaultState()), new SimpleBlockPlacer())).tries(64)
+							.build())
+					.withPlacement(Features.Placements.PATCH_PLACEMENT).func_242731_b(0);
+			event.getRegistry().register(feature.setRegistryName("desolate_vines"));
+			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("dystopiacraft:desolate_vines"), configuredFeature);
+		}
+	}
+	@SubscribeEvent
+	public void addFeatureToBiomes(BiomeLoadingEvent event) {
+		event.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION).add(() -> configuredFeature);
+	}
+	public static class BlockCustomFlower extends SugarCaneBlock {
+		public BlockCustomFlower() {
+			super(Block.Properties.create(Material.PLANTS).tickRandomly().doesNotBlockMovement().sound(SoundType.PLANT).hardnessAndResistance(0f, 0f)
+					.setLightLevel(s -> 0));
+			setRegistryName("desolate_vines");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 100;
+		}
+
+		@Override
+		public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 60;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(Blocks.AIR, (int) (1)));
+		}
+
+		@Override
+		public PlantType getPlantType(IBlockReader world, BlockPos pos) {
+			return PlantType.PLAINS;
+		}
+
+		@Override
+		public void tick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+			if (!state.isValidPosition(world, pos)) {
+				world.destroyBlock(pos, true);
+			} else if (world.isAirBlock(pos.up())) {
+				int i = 1;
+				for (; world.getBlockState(pos.down(i)).getBlock() == this; ++i);
+				if (i < 3) {
+					int j = state.get(AGE);
+					if (j == 15) {
+						world.setBlockState(pos.up(), getDefaultState());
+						world.setBlockState(pos, state.with(AGE, 0), 4);
+					} else {
+						world.setBlockState(pos, state.with(AGE, j + 1), 4);
+					}
+				}
+			}
+		}
+	}
+}
